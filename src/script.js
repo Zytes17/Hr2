385f0902d439b1da036258642b29d6ace7519706
Zytes17 <jorishrko@gmail.com>
2025-01-29 17:01:04 +0800

    first commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+
+document.getElementById("notificationToggle").addEventListener("click", function () {
+    document.getElementById("notificationMenu").classList.toggle("hidden");
+});
+
+function toggleProfileDropdown() {
+    document.getElementById("profileDropdownMenu").classList.toggle("hidden");
+}
+
+
+//SidebarPC
+function onMenu() {
+    document.getElementById("sidebar").classList.toggle("-translate-x-full");
+}
+
+//SidebarMobile
+function togglePhoneSidebar() {
+    document.getElementById("phonebar").classList.toggle("-translate-x-full");
+}
+
+
+
+
+
